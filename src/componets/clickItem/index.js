94a17655da92d3ac4a2ac2127feab9369874a75d
Chong Thao <tsoogthoj@hotmail.com>
2019-01-24 18:52:27 -0800

    fixed img positioning

--- src/componets/clickItem/index.js
+++ src/componets/clickItem/index.js
@@ -4,8 +4,6 @@ import "./style.css";
 function ClickImg(props) {
   return (
     <div
-      role="img"
-      aria-label="click item"
       onClick={() => props.handleClick(props.id)}
       style={{ backgroundImage: `url("${props.image}")` }}
       className={`click-item`}
